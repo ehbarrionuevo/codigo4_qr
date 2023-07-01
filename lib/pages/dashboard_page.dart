a3a0f70b3e67e81a19bf2a3b8b3620472e68c349
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-07-01 10:38:42 -0500

    Mods

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
+
+import '../models/qr_model.dart';
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
@@ -9,12 +12,34 @@ class DashboardPage extends StatefulWidget {
 }
 
 class _DashboardPageState extends State<DashboardPage> {
-  List<ChartData> data = [
-    ChartData(titleX: "Pedro", valueY: 30),
-    ChartData(titleX: "Juan", valueY: 50),
-    ChartData(titleX: "Fidel", valueY: 70),
-    ChartData(titleX: "Ramón", valueY: 55),
-  ];
+  List<ChartData> data = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  Future<void> getData() async {
+    CollectionReference qrCollection =
+        FirebaseFirestore.instance.collection("qr_collection");
+    QuerySnapshot collection = await qrCollection.get();
+    List<QueryDocumentSnapshot> docs = collection.docs;
+    List<QrModel> qrList = [];
+    qrList = docs
+        .map((e) => QrModel.fromJson(e.data() as Map<String, dynamic>))
+        .toList();
+
+    qrList.forEach((element) {
+      data.add(
+        ChartData(
+          titleX: element.description,
+          valueY: element.price,
+        ),
+      );
+    });
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,12 +56,35 @@ class _DashboardPageState extends State<DashboardPage> {
             SfCartesianChart(
               primaryXAxis: CategoryAxis(),
               primaryYAxis: NumericAxis(minimum: 0, maximum: 100, interval: 20),
-              series: [
-                ColumnSeries(
+              tooltipBehavior: TooltipBehavior(
+                enable: true,
+              ),
+              series: <ChartSeries<ChartData, String>>[
+                ColumnSeries<ChartData, String>(
                   dataSource: data,
                   xValueMapper: (ChartData temp, index) => temp.titleX,
                   yValueMapper: (ChartData temp, index) => temp.valueY,
+                  name: 'Gold',
+                  color: Color.fromRGBO(8, 142, 255, 1),
                 ),
+              ],
+            ),
+            SfCircularChart(
+              tooltipBehavior: TooltipBehavior(
+                enable: true,
+              ),
+              legend: const Legend(
+                isVisible: true,
+                title: LegendTitle(
+                  text: "Data general",
+                ),
+              ),
+              series: <CircularSeries>[
+                DoughnutSeries<ChartData, String>(
+                    dataSource: data,
+                    // pointColorMapper: (ChartData data, _) => Color,
+                    xValueMapper: (ChartData data, _) => data.titleX,
+                    yValueMapper: (ChartData data, _) => data.valueY)
               ],
             ),
           ],
