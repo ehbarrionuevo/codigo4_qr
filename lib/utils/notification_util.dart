81e487cc896fb7d1b375f620cedde809061c223d
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-07-01 12:19:42 -0500

    Mods

--- lib/utils/notification_util.dart
+++ lib/utils/notification_util.dart
@@ -6,21 +6,31 @@ class NotificationUtil {
   static initMessaging() async {
     String token = await firebaseMessaging.getToken() ?? "-";
     print(token);
-
+    firebaseMessaging.subscribeToTopic("MANDARINA");
+    firebaseMessaging.subscribeToTopic("MANTEQUILLA");
     FirebaseMessaging.onMessage.listen(_onMessage);
     FirebaseMessaging.onBackgroundMessage(_onBackgroundMessage);
     FirebaseMessaging.onMessageOpenedApp.listen(_onMessageOpenedApp);
   }
 
   static _onMessage(RemoteMessage message) async {
-    print(message);
+    if (message.notification != null) {
+      print(message.notification!.title);
+      print(message.notification!.body);
+    }
   }
 
   static Future _onBackgroundMessage(RemoteMessage message) async {
-    print(message);
+    if (message.notification != null) {
+      print(message.notification!.title);
+      print(message.notification!.body);
+    }
   }
 
   static _onMessageOpenedApp(RemoteMessage message) async {
-    print(message);
+    if (message.notification != null) {
+      print(message.notification!.title);
+      print(message.notification!.body);
+    }
   }
 }
